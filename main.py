0e19312fc801dda3f2a537349c85fd2d730b7901
unknown <mabg0630@gmail.com>
2025-04-09 22:36:53 -0500

    tr

--- main.py
+++ main.py
@@ -107,4 +107,4 @@ def cleanup_temp_files(file_paths):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8002, reload=True)
